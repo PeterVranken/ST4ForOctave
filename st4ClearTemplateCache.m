bd957c599a3ac98d6dd1d36561bdb674a2538a81
vranken <vranken@c55333f4-d057-4fd1-a428-ba6749bcbd31>
2020-03-26 21:22:20 +0000

    The library ST4ForOctave had been published under the terms of the GNU Lesser Public License but several scripts still had inside the copyright notice of the GPL

--- st4ClearTemplateCache.m
+++ st4ClearTemplateCache.m
@@ -24,20 +24,20 @@ function st4ClearTemplateCache
 %       st4ClearTemplateCache
 %       text = st4Render('helloWorld.stg', 'myHelloWorldTemplate', 'greeting', 'Hello', 'name', 'World')
 %
-%   Copyright (C) 2018 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
+%   Copyright (C) 2018-2020 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
 %
 %   This program is free software: you can redistribute it and/or modify it
-%   under the terms of the GNU General Public License as published by the
-%   Free Software Foundation, either version 3 of the License, or (at your
-%   option) any later version.
-%
-%   This program is distributed in the hope that it will be useful, but
-%   WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-%   General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License along
-%   with this program. If not, see <http://www.gnu.org/licenses/>.
+%   under the terms of the GNU Lesser General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or any later
+%   version.
+%  
+%   This program is distributed in the hope that it will be useful, but WITHOUT
+%   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+%   for more details.
+%  
+%   You should have received a copy of the GNU Lesser General Public License
+%   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
     info.doClearCache = true;
     info.outputFileName = [];
--- st4RenderWrite.m
+++ st4RenderWrite.m
@@ -49,20 +49,20 @@ function st4RenderWrite( fileName               ...
 %       st4RenderWrite('helloWorld.txt', true, 'helloWorld.stg', 'myHelloWorldLstTemplate', 4, 'greeting', 'Hello', 'nameList', {'World', 'Anna', 'Tom', 'Terence Parr'})
 %       edit helloWorld.txt
 %
-%   Copyright (C) 2018 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
+%   Copyright (C) 2018-2020 Peter Vranken (mailto:Peter_Vranken@Yahoo.de)
 %
 %   This program is free software: you can redistribute it and/or modify it
-%   under the terms of the GNU General Public License as published by the
-%   Free Software Foundation, either version 3 of the License, or (at your
-%   option) any later version.
-%
-%   This program is distributed in the hope that it will be useful, but
-%   WITHOUT ANY WARRANTY; without even the implied warranty of
-%   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-%   General Public License for more details.
-%
-%   You should have received a copy of the GNU General Public License along
-%   with this program. If not, see <http://www.gnu.org/licenses/>.
+%   under the terms of the GNU Lesser General Public License as published by the
+%   Free Software Foundation, either version 3 of the License, or any later
+%   version.
+%  
+%   This program is distributed in the hope that it will be useful, but WITHOUT
+%   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+%   FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+%   for more details.
+%  
+%   You should have received a copy of the GNU Lesser General Public License
+%   along with this program. If not, see <http://www.gnu.org/licenses/>.
 
     % TODO Allow having verbose as string, too
 
